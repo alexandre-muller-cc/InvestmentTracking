42f90e4cefe256de6c28f676e8a576e868083511
sanji <sanji@kali>
2024-07-13 10:45:16 -0400

    test

---
+++ tictacto/src/tictacto/driver.java
@@ -0,0 +1,15 @@
+package tictacto;
+
+public class driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] moves = {1,2,3,4,5,6,7};
+		int[] test = Tictacto.playermove(moves);
+				
+		
+
+	}
+
+}
